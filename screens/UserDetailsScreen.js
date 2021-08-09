aa973f81b9f4dc57e5fb8863031de1f5ec2f3e50
Pamela Nahum <pamela.nahum@agrosat.cl>
2021-08-09 09:59:38 -0400

    eliminar y modificar usuarios

--- screens/UserDetailsScreen.js
+++ screens/UserDetailsScreen.js
@@ -1,41 +1,109 @@
-import React, {useEffect} from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Button, TextInput, ScrollView, StyleSheet, ActivityIndicator, Alert  } from 'react-native';
 import firebase from '../database/firebase';
 
 const UserDetailsScreen = (props) =>{
 
+    //se creó esta funcion para no tener que escribir el objeto vacio mil veces
+    const objetoInicial={
+        id:"",
+        name:"",
+        correo:"",
+        telefono:"",
+
+    }
+    //con esto se hace un "cargando"
+    const [loading, setLoading] = useState(true)
+
+    //con esto tomamos el usuario vacio y lo alistamos para tomar parámetros
+    const [user, setUser] = useState(objetoInicial)
+    
+    
+
     const getUserById = async(id) => {
+        //aqui yo recibo el id que envíe como parámetro en la vista anterior
         const dbRef= firebase.db.collection("users").doc(id)
         const doc = await dbRef.get();
         const user =doc.data();
-    }
+        //aqui yo ya comienzo a actualizar al usuario
+        setUser({
+            ...user,
+            id:doc.id,
+        });
+        setLoading(false)
+    };
 
     useEffect(()=>{
         getUserById(props.route.params.user_id);
-    })
+    },[])
+
+    const handleChangeText = (name, value) =>{
+        setUser({...user, [name]:value})
+    };
+    //en esta fucion genero el dele en la base para eliminar usuarios
+    const eliminarUsuario = async()=>{
+        const dbRef = firebase.db.collection('users').doc(props.route.params.user_id);
+        dbRef.delete();
+        props.navigation.navigate('UsersList')
+    };
+    //en esta fucion genero el dele en la base para modificar usuarios
+    const modificarUsuario = async()=>{
+        const dbRef = firebase.db.collection('users').doc(user.id);
+        await dbRef.set({
+            name: user.name,
+            correo: user.correo,
+            telefono: user.telefono
+        })
+        setUser(objetoInicial)
+        props.navigation.navigate('UsersList')
+    }
+    
+    const confEliminar = () => {
+        Alert.alert("Eliminar Usuario", "Está seguro que desea eliminar?",[
+            {text: 'Yes', onPress:()=>eliminarUsuario()},
+            {text: 'No', }
+        ])
+    }
+
+
+    if(loading){
+        return(
+            <View>
+                <ActivityIndicator size="large"/>
+            </View>
+        )
+    };
+
 
 
     
     return(
+        //cuada parámetro que logro obtener de lo de arriba se lo asigno a un cuadro de texto
         <ScrollView style={styles.container}>
             <View style={styles.inputGroup}> 
                 <TextInput 
                 placeholder="Nombre de usuario"
+                value={user.name}
                 onChangeText={(value)=>handleChangeText('name', value)}
                 />
             </View>
             <View style={styles.inputGroup}>
                 <TextInput 
                 placeholder="Correo"
+                value={user.correo}
                 onChangeText={(value)=>handleChangeText('correo', value)}
                 />
             </View>
             <View style={styles.inputGroup}>
                 <TextInput 
                 placeholder="Telefono"
+                value={user.telefono}
                 onChangeText={(value)=>handleChangeText('telefono', value)}/>
             </View>
-            <Button title="Guardar Usuario" onPress={()=> createNewUser()}/>
+            <Button title="Guardar Usuario" onPress={()=> modificarUsuario()}/>
+            <Button 
+            color="red"
+            title="Eliminar Usuario" onPress={()=> confEliminar()}/>
 
         </ScrollView>
         
--- screens/UsersList.js
+++ screens/UsersList.js
@@ -5,8 +5,11 @@ import firebase from '../database/firebase';
 import { ListItem, Avatar } from 'react-native-elements';
 
 const UsersList = (props) =>{
+    //aqui se rea la lista que va a guardar los datos que llegen de la base
 
     const [users, setUsers]= useState([]);
+
+    //Con esto hago la consulta a la base y lleno la lista con los datos
 
     useEffect(()=>{
         firebase.db.collection("users").onSnapshot((querySnapshot)=>{
@@ -34,6 +37,10 @@ const UsersList = (props) =>{
                 users.map(user => {
                     return(
                         <ListItem
+                        /*aqui utilizo un boton tipo "flechita" para poder ingresa a cada usuario
+                        la idea es que despues pueda tomar los datos
+                        y modificarlos en otra vista, ademas le envío como parámetro el id del usuario
+                        seleccionado*/
                         key={user.id} bottomDivider onPress={()=>{
                             props.navigation.navigate('UserDetailsScreen',{
                                 user_id:user.id
